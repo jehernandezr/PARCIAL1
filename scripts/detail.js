04fc85b3d2b73934beff12ea23b7612fe1760957
JONATAN ESMITS HERNANDEZ RUBIO+ <je.hernandezr@uniandes.edu.co>
2021-03-13 11:52:04 -0500

    mitad parcial

---
+++ scripts/detail.js
@@ -0,0 +1,53 @@
+function formatCurrency(locales, currency, fractionDigits, number) {
+  let formatted = new Intl.NumberFormat(locales, {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: fractionDigits,
+  }).format(number);
+  return formatted;
+}
+function loadDetail() {
+  const element = JSON.parse(localStorage.getItem("seleccionado"));
+  const oldCard =document.querySelector(".card-large");
+  let image = element.picture;
+  let id = element.id;
+  let title = element.title;
+  let price = formatCurrency(
+    "es-AR",
+    element.price.currency,
+    2,
+    element.price.amount,
+  );
+  let description= element.description;
+  let units = element.sold_quantity;
+  let condition = element.condition;
+  
+  create_elementDetail(id,title,description,units,condition,price,image,oldCard);    
+}
+
+function create_elementDetail(id,title,description,units,condition,price,image,oldCard) {
+  let Newcard = oldCard.cloneNode(true);
+  Newcard.setAttribute("id",id);
+  img=Newcard.children[0].children[0];
+  img.setAttribute("src",image);
+  uni_usad= Newcard.children[1].children[0];
+  uni_usad.innerHTML=condition+ " | "+units+"  Vendidos";
+  title_t=Newcard.children[1].children[1];
+  title_t.innerHTML=title.substr(0,24);
+  money=Newcard.children[1].children[2];
+  money.innerHTML=price;
+  desc_par=Newcard.children[2].children[1];
+  desc_par.innerHTML=description;
+
+  oldCard.parentNode.replaceChild(Newcard,oldCard);
+
+}
+function search(event){
+    event.preventDefault();
+    buscar= document.querySelector(".form input")[0].value;
+    console.log(buscar);
+
+}
+
+loadDetail();
+
---
+++ scripts/main.js
@@ -0,0 +1,68 @@
+function formatCurrency(locales, currency, fractionDigits, number) {
+  let formatted = new Intl.NumberFormat(locales, {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: fractionDigits,
+  }).format(number);
+  return formatted;
+}
+
+function mostrardata(data) {
+  localStorage.setItem("productos",JSON.stringify(data.items));
+  data.items.forEach((element) => {
+    let image = element.picture;
+    let id = element.id;
+    let title = element.title;
+    let price = formatCurrency(
+      "es-AR",
+      element.price.currency,
+      2,
+      element.price.amount,
+    );
+    let location = element.location;
+    let is_freeShipping = element.free_shipping;
+    let old_card=document.querySelector(".card");
+    let Newcard = old_card.cloneNode(true);
+    crear_elements(id,image,price,title,location,is_freeShipping,Newcard);    
+  });
+
+  document.getElementById("products").removeChild(document.getElementById("1"));
+
+}
+
+fetch(
+  "https://gist.githubusercontent.com/jhonatan89/719f8a95a8dce961597f04b3ce37f97b/raw/4b7f1ac723a14b372ba6899ce63dbd7c2679e345/products-ecommerce"
+)
+  .then((response) => response.json())
+  .then(mostrardata)
+  .catch((err) => (err.message));
+
+
+function crear_elements (id,image, price,title,location, is_freeShipping, new_card) {
+  card_image=new_card.children[0];
+  card_content= new_card.children[1];
+  img=card_image.children[0];
+  price_span=card_content.children[0];
+  img_shiping=price_span.children[0];
+  city_span= card_content.children[1];
+  model=card_content.children[2];
+  
+  new_card.setAttribute("id",id );
+  img.setAttribute("src",image);
+  price_span.innerHTML=price;
+  if(is_freeShipping) {price_span.appendChild(img_shiping);}
+  city_span.innerHTML=location;
+  model.innerHTML=title;
+  
+  document.getElementById("products").appendChild(new_card);
+}
+
+// eslint-disable-next-line no-unused-vars
+function irdetail(este) {
+  let id_product=este.parentNode.parentNode.id;
+  let f = JSON.parse(localStorage.getItem("productos"));
+  let mi_product = f.find((item) => {return item.id === id_product;});
+  localStorage.setItem("seleccionado",JSON.stringify(mi_product));
+  window.location="detail.html";
+
+}
